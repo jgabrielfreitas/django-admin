4dc4475a8ca9a4839a3e3135ee72687f060f952d
JGabrielFreitas <jgabrielafreitas@gmail.com>
2016-11-05 00:50:52 -0200

    view hello world

--- django_admin_site/polls/views.py
+++ django_admin_site/polls/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def hello_world(request):
+    return HttpResponse("Hello world! My test is working")
